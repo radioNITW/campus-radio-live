47dabf48f8db9e947dca80ad05fcdf777ee9dd27
Aakash074 <aakashkumaar074@gmail.com>
2021-03-04 16:50:58 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# campus-radio-live
+<h1 align="center">Campus Radio Live</h1>
+
+This is completely open-source and the only third-party service it uses is the [iFrame API by Google](https://developers.google.com/youtube/iframe_api_reference) to control the videos on the background. Please enjoy the app [here](#).
+
+# Setup
+
+1. Clone this project
+2. Open the `index.html`
+3. Great. You have your own Walk and Listen running locally.
+
+If you need to add more locations locally go to the file `js/locations.js` and increment there with all the respective properties: `id`, `name`, `videoId` and `radio`.
+
+# Magic behind this
+
+To begin with, a huge thank you to (...) for providing high quality walking tours on cities from all around the globe. This is where the videos where scrapped from and implemented on the website.
+
+### How did I get the radio urls?
+
+Simply by looking at the source code of a great website, [radiowebsites.org](https://radiowebsites.org/), which provides a lot of radios from around the world and at their page source code they call this api where they get the source url of the radios.
+
+### How did I cancel the play/pause button of youtube?
+
+I just add a simple line of css code to the iframe player
+
+```css
+.player {
+	pointer-events: none;
+}
+```
+
+### How did I remove the youtube information such as youtube channel title and etc
+
+Just adding a height of 120% and removing the overflow of the page will remove the top and bottom youtube extra controllers.
+
+* You can see more of the magic by looking at the source code.
---
+++ js/locations.js
@@ -0,0 +1,85 @@
+LOCATIONS = [ // http://stream1.bestfmbudapest.hu/bestfm_nyiregyhaza.mp3
+	{
+		"id": 3,
+		"name": "NIT, Warangal",
+		"videoId": "iGeuCueiYgc",
+		"radio": {
+			"url": "http://radio.staychu.com/Arijit%20Singh%20-%20Dekh%20Lena.mp3",
+			"name": "NIT Warangal songs"
+		},
+		"startTime": 13
+	},
+	{
+		"id": 10,
+		"name": "IIT, Kanpur",
+		"videoId": "tZWW3mVp6MA",
+		"radio": {
+			"url": "http://stream1.bestfmbudapest.hu/bestfm_nyiregyhaza.mp3",
+			"name": "IIT Kanpur"
+		},
+		"startTime": 29
+	},
+	{
+		"id": 7,
+		"name": "NIT, Trichy",
+		"videoId": "SLKgz63zK5s",
+		"radio": {
+			"url": "https://au1.fastcast4u.com/proxy/dzcwbfih_relay?mp=/1",
+			"name": "NIT Trichy"
+		},
+		"startTime": 57
+	},
+	{
+		"id": 9,
+		"name": "IIT, Delhi",
+		"videoId": "6Pgyr0H1XLQ",
+		"radio": {
+			"url": "https://playerservices.streamtheworld.com/api/livestream-redirect/977_HITSAAC_SC",
+			"name": "IIT Delhi"
+		},
+		"startTime": 01
+	},
+	{
+		"id": 4,
+		"name": "AIIMS, Delhi",
+		"videoId": "4JBI2RCmAII",
+		"radio": {
+			"url": "https://ice08.fluidstream.net/ibiza.aac",
+			"name": "AIIMS Delhi"
+		},
+		"startTime": 10
+	},
+	{
+		"id": 6,
+		"name": "IIM, Bangalore",
+		"videoId": "yyXDs7LZpz8",
+		"radio": {
+			"url": "https://cadena100-cope-rrcast.flumotion.com/cope/cadena100-low.mp3",
+			"name": "IIM Bangalore"
+		},
+		"startTime": 19
+	}
+]
+
+function chooseRandomLocation() {
+	const availableLocations = LOCATIONS.filter(({id}) => state.currentLocation ? state.currentLocation.id !== id : true);
+	
+	return availableLocations[Math.floor(Math.random() * availableLocations.length)];
+}
+
+function changeLocation(locationId) {
+	if (state.currentLocation && locationId == state.currentLocation.id)
+		return;
+
+	changeLoadingState(true);
+
+	const location = LOCATIONS.find(location => location.id == locationId);
+
+	state.currentLocation = location;
+
+	changeLocationText(location.name);
+	changeVideoSource(location.videoId);
+	changeRadio(location.radio);
+	toggleActiveClassOnLocation(location.id);
+	playVideo(location);
+}
